19e8f2521956a7d631230c595163b80c79e7f2d0
chrzanof <58990661+chrzanof@users.noreply.github.com>
2025-05-05 23:31:40 +0200

    Add project files.

---
+++ HealthTracker/HealthTracker.cpp
@@ -0,0 +1,27 @@
+﻿#include "raylib.h"
+
+int main(void)
+{
+    
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Health Tracker");
+
+    SetTargetFPS(60);              
+
+    while (!WindowShouldClose())   
+    {
+        BeginDrawing();
+
+        ClearBackground(DARKGRAY);
+
+        DrawText("Hello, Window!", 320, 200, 20, RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
